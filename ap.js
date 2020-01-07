7424520c4b7c9290bee7a756e49d4bec923265b3
advaya-website <59248086+advaya-website@users.noreply.github.com>
2020-01-07 08:48:30 +0530

    Update ap.js

--- ap.js
+++ ap.js
@@ -42,7 +42,7 @@ function myFunction(item, index) {
         caname: form.caname.value,
         college: form.clgName.value,
         collegeid: form.clgId.value,
-        //email: form.email.value,
+        email: form.email.value,
         phone: form.phone.value,
         promo: promo.concat(y.slice(0,3).toUpperCase()),
         mailsent: false,
