f3667b617e156543f9411ba0d9d4ae3b0143adec
advaya-website <59248086+advaya-website@users.noreply.github.com>
2020-01-09 23:22:59 +0530

    Update p.js

--- parallaxo/p.js
+++ parallaxo/p.js
@@ -1,10 +1,12 @@
 const form = document.querySelector('#para');
 form.addEventListener('submit', (e) => {
+    count++;
     alert("Round two will be live on 19012020");
     e.preventDefault();
     db.collection('parallaxo').doc(form.pno.value).set({
         name: form.pname.value,
         phone: form.pno.value,
+        count:count,
     });
     form.name.value = '';
     form.pno.value = '';
