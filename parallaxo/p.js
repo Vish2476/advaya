9297fc14bd2adb74686a129e9a87a6a652d4f8e7
advaya-website <59248086+advaya-website@users.noreply.github.com>
2020-01-10 23:38:34 +0530

    Update p.js

--- parallaxo/p.js
+++ parallaxo/p.js
@@ -4,7 +4,7 @@ var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds(
 form.addEventListener('submit', (e) => {
     alert("You will be notified when round two goes live");
     e.preventDefault();
-    db.collection('parallaxoday2').doc(form.pno.value).set({
+    db.collection('parallaxo').doc(form.pno.value).set({
         name: form.pname.value,
         phone: form.pno.value,
         time:time,
