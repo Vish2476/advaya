750fcc5d774d273099362a3066b6ad65aa9fb17a
advaya-website <59248086+advaya-website@users.noreply.github.com>
2020-01-11 15:28:33 +0530

    Update p.js

--- parallaxo/p.js
+++ parallaxo/p.js
@@ -1,15 +1,4 @@
 
-
-
-function getIPAddress{
-    $.getJSON("https://jsonip.com?callback=?", function(data) {
-        var a=data.ip;
-        alert(a);
-        return a;
-    });
-  };
-
-
 const form = document.querySelector('#para');
 var today = new Date();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
@@ -18,11 +7,10 @@ var ipa= getIPAddress();
 form.addEventListener('submit', (e) => {
     alert("You will be notified when round two goes live");
     e.preventDefault();
-    db.collection('ptest').doc(form.pno.value).set({
+    db.collection('parallaxoday2').doc(form.pno.value).set({
         name: form.pname.value,
         phone: form.pno.value,
         time:Date().toString().substring(0, 24),
-        ip:ipa.toString().substring(0, 24),
     });
     form.name.value = '';
     form.pno.value = '';
