189d1721dc07fd081f552aa1d6e7affc7e889ac8
advaya-website <59248086+advaya-website@users.noreply.github.com>
2020-01-11 14:50:20 +0530

    ss

--- parallaxo/p.js
+++ parallaxo/p.js
@@ -1,13 +1,28 @@
 const form = document.querySelector('#para');
 var today = new Date();
 var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+/* Add "https://api.ipify.org?format=json" statement 
+		this will communicate with the ipify servers in 
+		order to retrieve the IP address $.getJSON will 
+		load JSON-encoded data from the server using a 
+		GET HTTP request */ 
+
+		$.getJSON("https://api.ipify.org?format=json", 
+										function(data) { 
+
+			// Setting text of element P with id gfg 
+			$("#gfg").html(data.ip); 
+		})
+
 form.addEventListener('submit', (e) => {
     alert("You will be notified when round two goes live");
     e.preventDefault();
-    db.collection('parallaxoday2').doc(form.pno.value).set({
+    db.collection('parallaxotest').doc(form.pno.value).set({
         name: form.pname.value,
         phone: form.pno.value,
         time:Date().toString().substring(0, 24),
+        ip:gfg,
     });
     form.name.value = '';
     form.pno.value = '';
