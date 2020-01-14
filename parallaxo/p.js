2b81b3ce65303d8cdb8db9637ab50f652a55095b
advaya-website <59248086+advaya-website@users.noreply.github.com>
2020-01-15 01:08:26 +0530

    asdads

--- parallaxo/p.js
+++ parallaxo/p.js
@@ -1,18 +1,27 @@
 
-const form = document.querySelector('#para');
-var today = new Date();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-form.addEventListener('submit', (e) => {
-    alert("You will be notified when round two goes live");
-    e.preventDefault();
-    db.collection('parallaxoday2').doc(form.pno.value).set({
-        name: form.pname.value,
-        phone: form.pno.value,
-        time:Date().toString().substring(0, 24),
-    });
-    form.name.value = '';
-    form.pno.value = '';
-    //window.location.replace("hunt1.html");
-});
 //window.location.replace("hunt1.html");
+$.getJSON("https://api.ipify.org?format=json", 
+function(data) { 
+
+    const form = document.querySelector('#para');
+    var today = new Date();
+    var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var address= data.ip;
+    
+    form.addEventListener('submit', (e) => {
+        alert("You will be mailed when round three goes live");
+        e.preventDefault();
+        db.collection('phunt2').doc(form.pno.value).set({
+            name: form.pname.value,
+            phone: form.pno.value,
+            time:Date().toString().substring(0, 24),
+            code:form.crackcode.value,
+            ip:address,
+        });
+        form.name.value = '';
+        form.pno.value = '';
+        form.crackcode.value='';
+        //window.location.replace("hunt1.html");
+    });
+
+}) 
